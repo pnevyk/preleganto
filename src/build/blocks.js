b1fc6e7e92cdb93f8cab2d12dae545b10d231b82
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2017-08-24 21:32:46 +0200

    Map commonly used programming languages aliases to correct names
    I found very common that I use abbreviation `js` for JavaScript and I
    am sure there are some alises for other languages too. This function
    should map all aliases (now it's just `js`) to corresponding language
    names.

--- src/build/blocks.js
+++ src/build/blocks.js
@@ -2,7 +2,7 @@
 
 import type { NodeSpecialBlock } from '../syntax/parse';
 
-import { highlight, supports } from '../prism';
+import { highlight, supports, mapAliases } from '../prism';
 import katex from 'katex';
 
 import { warn } from '../logger';
@@ -21,7 +21,7 @@ export default function (block: NodeSpecialBlock): string {
 
 function source(block: NodeSpecialBlock): string {
     if (block.args.length > 0) {
-        const language = block.args[0];
+        const language = mapAliases(block.args[0]);
         if (supports(language)) {
             const highlighted = highlight(block.value, language);
             return `<pre class="preleganto-source"><code class="language-${language}">${highlighted}</code></pre>`;
